72c41427a67ab8870a5e8a9c5a0cc9886015a966
Ange Patrice Lenoir Adou <41445736+pascallenoir@users.noreply.github.com>
2023-05-22 12:23:04 +0200

    #ange adou stars rating on footer (#6)
    * Ajout d'elements dans le footer
    
    * modifications mineures
    
    ---------
    
    Co-authored-by: CERGIBS\ange.adou <ange.adou@cergibs.com>

--- components/footer.js
+++ components/footer.js
@@ -3,179 +3,239 @@ const footerTemplate = document.createElement('template');
 
 footerTemplate.innerHTML = `
 <style>
+
 body {
-    padding: 0;
-    margin: 0;
-    height: 100%;
-    display: flex;
-    align-items: flex-end;
-  }
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+}
 
 footer {
-    background-color: #34EDC2;
-    color: black;
-    font-size: 16px;
+  background-color: #34EDC2;
+  color: black;
+  font-size: 16px;
+}
+footer * {
+  font-family: "latoregular", "sans-serif";
+  box-sizing: border-box;
+  border: none;
+  outline: none;
+}
+.row {
+  padding: 1em 1em;
+}
+.row.primary {
+  display: grid;
+  grid-template-columns: 4fr 3fr 3fr;
+  align-items: strech;
+}
+.column {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 2em;
+  min-height: 15em;
+}
+h3 {
+  width: 100%;
+  text-align: left;
+  color: black;
+  font-size: 1.4em;
+  white-space: nowrap;
+}
+h1 {
+  width: 100%;
+  text-align: left;
+  color: white;
+  font-size: 1.4em;
+  white-space: nowrap;
+}
+ul {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  margin: 0;
+}
+li:not(:first-child) {
+  margin-top: 0.8em;
+}
+ul li a {
+  color: black;
+  text-decoration: none;
+}
+ul li a:hover {
+  color: #2a8ded;
+}
+
+.about p {
+  text-align: justify;
+  line-height: 2;
+  margin: 0;
+}
+input,
+button {
+  font-size: 1em;
+  padding: 1em;
+  width: 100%;
+  border-radius: 5px;
+  margin-bottom: 5px;
+}
+button {
+  background-color: #c7940a;
+  color: #ffffff;
+}
+div.social {
+  display: flex;
+  justify-content: space-around;
+  font-size: 2.4em;
+  flex-direction: row;
+  margin-top: 0.5em;
+}
+.social i {
+  color: #bac6d9;
+}
+
+.copyright {
+  padding: 0.3em 1em;
+  background-color: #25262e;
+}
+
+.copyright p {
+  font-size: 0.9em;
+  text-align: right;
+  color: white;
+}
+
+.Stars {
+  --percent: calc(var(--rating) / 5 * 100%);
+  display: inline-block;
+  font-size: var(--star-size);
+  font-family: Times; /*make sure ★ appears correctly */
+  line-height: 1;
+  border-radius: 3px;
+  border: 0.1rem solid #000;
+  background-color: var(--star-backgroundcard);
+  
+  &::before {
+    content: '★★★★★';
+    letter-spacing: 3px;
+    background: linear-gradient(90deg, var(--star-background) var(--percent), var(--star-color) var(--percent));
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
   }
-  footer * {
-    font-family: "latoregular", "sans-serif";
-    box-sizing: border-box;
-    border: none;
-    outline: none;
-  }
-  .row {
-    padding: 1em 1em;
-  }
+}
+
+.Rating {
+  display: inline-block;
+  font-size: var(--star-size);
+  font-family: Times; /*make sure ★ appears correctly */
+  border-radius: 3px;
+  padding: 0.2em;
+  border: 0.1rem solid #000;
+  background-color: #34EDC2;
+
+}
+.Ratingp{
+  font-size: 0.7em;
+  color: #000;
+  margin: 0;
+
+}
+
+
+@media screen and (max-width: 992px) {
   .row.primary {
     display: grid;
     grid-template-columns: 4fr 3fr 3fr;
     align-items: strech;
   }
-  .column {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    padding: 0 2em;
-    min-height: 15em;
-  }
-  h3 {
-    width: 100%;
-    text-align: left;
-    color: black;
-    font-size: 1.4em;
-    white-space: nowrap;
-  }
-  h1 {
-    width: 100%;
-    text-align: left;
-    color: white;
-    font-size: 1.4em;
-    white-space: nowrap;
-  }
-  ul {
-    list-style: none;
-    display: flex;
-    flex-direction: column;
-    padding: 0;
-    margin: 0;
-  }
-  li:not(:first-child) {
-    margin-top: 0.8em;
-  }
-  ul li a {
-    color: black;
-    text-decoration: none;
-  }
-  ul li a:hover {
-    color: #2a8ded;
-  }
-  .about p {
-    text-align: justify;
-    line-height: 2;
-    margin: 0;
-  }
-  input,
-  button {
-    font-size: 1em;
-    padding: 1em;
-    width: 100%;
-    border-radius: 5px;
-    margin-bottom: 5px;
-  }
-  button {
-    background-color: #c7940a;
-    color: #ffffff;
-  }
-  div.social {
-    display: flex;
-    justify-content: space-around;
-    font-size: 2.4em;
-    flex-direction: row;
-    margin-top: 0.5em;
-  }
-  .social i {
-    color: #bac6d9;
-  }
-  
-  .copyright {
-    padding: 0.3em 1em;
-    background-color: #25262e;
-  }
- 
-  .copyright p {
-    font-size: 0.9em;
-    text-align: right;
-    color: white;
-  }
-  @media screen and (max-width: 992px) {
-    .row.primary {
-      display: grid;
-      grid-template-columns: 4fr 3fr 3fr;
-      align-items: strech;
-    }
-  
+
+}
+
+@media screen and (max-width: 768px) {
+  .row.primary {
+    grid-template-columns: 1fr;
   }
 
-  @media screen and (max-width: 768px) {
-    .row.primary {
-      grid-template-columns: 1fr;
-    }
-  
+}
+
+@media screen and (max-width: 576px) {
+  .row.primary {
+    grid-template-columns: 1fr;
   }
 
-  @media screen and (max-width: 576px) {
-    .row.primary {
-      grid-template-columns: 1fr;
-    }
-  
-  }
+}
 
 </style>
 
+<!-- pieds de page du site internet mettant en avant les informations relatives a la dite societe -->
 <footer>
-<div class="row primary">
-    <div class="column about">
-
-        <h1>SIGITRAG</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
-        </p>
-    </div>
-
-    <div class="column links">
-        <h3>Statistiques</h3>
-    </div>
-
-    <div class="column links">
-        <h3>A propos de SIGITRAG</h3>
-        <ul>
+  <div class="row primary">
+      <div class="column about">
+  
+          <h1>SIGITRAG</h1>
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+          </p>
+      </div>
+  
+      <div class="column statistiques">
+          <h3>Statistiques</h3>
+          <ul>
             <li>
-              <a href="#faq">Devenir propriétaire</a>
+              <a href="#support">Services livrés :</a>
             </li>
             <li>
-              <a href="#cookies-policy">Nos Services</a>
+              <div class="Rating">
+                <p class="Ratingp">
+                  3265
+                </p>
+              </div>
             </li>
             <li>
-              <a href="#terms-of-services">Pourquoi nous choisir ?</a>
+              <a href="#support">Satisfaction client :</a>
             </li>
             <li>
-              <a href="#support">Nous Contacter</a>
+              <div class="Stars" style="--rating: 2.9;" aria-label="Rating of this product is 2.3 out of 5."></div>
             </li>
         </ul>
-    </div>
-</div>
-
-<div class="row copyright">
-    <div class="social">
-        <i class="fab fa-facebook-f"></i>
-        <i class="fab fa-instagram"></i>
-        <i class="fab fa-twitter"></i>
-        <i class="fab fa-whatsapp"></i>
-    </div>
-    <p>Copyright &copy; 2023 Bumpy Society</p>
-</div>
-</footer>
+      </div>
+  
+      <div class="column links">
+          <h3>A propos de SIGITRAG</h3>
+          <ul>
+              <li>
+                <a href="#faq">Devenir propriétaire</a>
+              </li>
+              <li>
+                <a href="#cookies-policy">Nos Services</a>
+              </li>
+              <li>
+                <a href="#terms-of-services">Pourquoi nous choisir ?</a>
+              </li>
+              <li>
+                <a href="#support">Notre localisation</a>
+              </li>
+          </ul>
+      </div>
+  </div>
+  
+  <div class="row copyright">
+      <div class="social">
+          <i class="fab fa-facebook-f"></i>
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-whatsapp"></i>
+      </div>
+      <p>Copyright &copy; 2023 Bumpy Society</p>
+  </div>
+  </footer>
 `;
 // fin Variable comprenant le contenu de code source du footer
 
